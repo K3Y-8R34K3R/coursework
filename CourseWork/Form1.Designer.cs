30bdaaedd6552e354a467203b6d0cd7d73ef61dd
vostro <vostro@K3Y_8R34K3R-PC>
2019-03-30 20:01:10 +0300

    Добавьте файлы проекта.

---
+++ CourseWork/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace CourseWork
+{
+    partial class Main_Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Exit_Btn = new System.Windows.Forms.Button();
+            this.Paint_btn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Exit_Btn
+            // 
+            this.Exit_Btn.Location = new System.Drawing.Point(57, 59);
+            this.Exit_Btn.Name = "Exit_Btn";
+            this.Exit_Btn.Size = new System.Drawing.Size(144, 49);
+            this.Exit_Btn.TabIndex = 0;
+            this.Exit_Btn.Text = "Exit";
+            this.Exit_Btn.UseVisualStyleBackColor = true;
+            this.Exit_Btn.Click += new System.EventHandler(this.Exit_Btn_Click);
+            // 
+            // Paint_btn
+            // 
+            this.Paint_btn.Location = new System.Drawing.Point(255, 59);
+            this.Paint_btn.Name = "Paint_btn";
+            this.Paint_btn.Size = new System.Drawing.Size(144, 49);
+            this.Paint_btn.TabIndex = 1;
+            this.Paint_btn.Text = "Paint";
+            this.Paint_btn.UseVisualStyleBackColor = true;
+            this.Paint_btn.Click += new System.EventHandler(this.Paint_btn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.No;
+            this.pictureBox1.Location = new System.Drawing.Point(30, 176);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Main_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 332);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Paint_btn);
+            this.Controls.Add(this.Exit_Btn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Main_Form_Load);
+            this.Resize += new System.EventHandler(this.Main_Form_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Exit_Btn;
+        private System.Windows.Forms.Button Paint_btn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ CourseWork/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace CourseWork
+{
+    public partial class Main_Form : Form
+    {
+        public Main_Form()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+        }
+
+        private void Paint_btn_Click(object sender, EventArgs e)
+        {
+            form_background_painter();
+        }
+        private void form_background_painter()
+        {
+            Graphics MyFormGrap = this.CreateGraphics();
+            LinearGradientBrush gradBrush = new LinearGradientBrush(ClientRectangle, Color.Green, Color.Blue, LinearGradientMode.Horizontal);
+            MyFormGrap.FillRectangle(gradBrush, ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
+            MyFormGrap.Dispose();
+        }
+
+        private void Main_Form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Exit_Btn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Main_Form_Resize(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+    }
+}
+
