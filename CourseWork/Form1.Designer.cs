34f12e545f4d8a14df33999b3f5f3629f2b957c2
vostro <vostro@K3Y_8R34K3R-PC>
2019-04-01 01:49:20 +0300

    Particles fade-out effect changed. Double bufferisation added

--- CourseWork/Form1.Designer.cs
+++ CourseWork/Form1.Designer.cs
@@ -29,31 +29,16 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.Exit_Btn = new System.Windows.Forms.Button();
-            this.Paint_btn = new System.Windows.Forms.Button();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.paintToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // Exit_Btn
-            // 
-            this.Exit_Btn.Location = new System.Drawing.Point(27, 12);
-            this.Exit_Btn.Name = "Exit_Btn";
-            this.Exit_Btn.Size = new System.Drawing.Size(144, 49);
-            this.Exit_Btn.TabIndex = 0;
-            this.Exit_Btn.Text = "Exit";
-            this.Exit_Btn.UseVisualStyleBackColor = true;
-            this.Exit_Btn.Click += new System.EventHandler(this.Exit_Btn_Click);
-            // 
-            // Paint_btn
-            // 
-            this.Paint_btn.Location = new System.Drawing.Point(207, 12);
-            this.Paint_btn.Name = "Paint_btn";
-            this.Paint_btn.Size = new System.Drawing.Size(144, 49);
-            this.Paint_btn.TabIndex = 1;
-            this.Paint_btn.Text = "Paint";
-            this.Paint_btn.UseVisualStyleBackColor = true;
-            this.Paint_btn.Click += new System.EventHandler(this.Paint_btn_Click);
             // 
             // flowLayoutPanel1
             // 
@@ -61,36 +46,90 @@
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(633, 251);
             this.flowLayoutPanel1.TabIndex = 3;
+            this.flowLayoutPanel1.Visible = false;
             // 
             // timer1
             // 
-            this.timer1.Interval = 10;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(659, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.paintToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // paintToolStripMenuItem
+            // 
+            this.paintToolStripMenuItem.Name = "paintToolStripMenuItem";
+            this.paintToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.paintToolStripMenuItem.Text = "Paint";
+            this.paintToolStripMenuItem.Click += new System.EventHandler(this.paintToolStripMenuItem_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(13, 28);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Minimum = 20;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(632, 45);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Value = 20;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
             // 
             // Main_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(659, 332);
+            this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.flowLayoutPanel1);
-            this.Controls.Add(this.Paint_btn);
-            this.Controls.Add(this.Exit_Btn);
+            this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Main_Form";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Main_Form_Load);
             this.Resize += new System.EventHandler(this.Main_Form_Resize);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button Exit_Btn;
-        private System.Windows.Forms.Button Paint_btn;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem paintToolStripMenuItem;
+        private System.Windows.Forms.TrackBar trackBar1;
     }
 }
 
--- CourseWork/Form1.cs
+++ CourseWork/Form1.cs
@@ -2,105 +2,147 @@
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Windows.Forms;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
 
 namespace CourseWork
 {
     public partial class Main_Form : Form
     {
+        const int particles_amount = 15; //количество кругов для отрисовки
+
         SolidBrush figure_brush;    //объект для заливки рисуемой фигуры
         Pen outline_pen;            //объект для обводки рисуемой фигуры
-        Graphics form_graphics;     //объект графики
+        BufferedGraphicsContext buff_graphics_context = BufferedGraphicsManager.Current;
+        BufferedGraphics buff_graphics;     //графический буфер
+        Graphics main_graphics;     //объект графики для отрисовки с привязкой к форме
 
-        int circle_radius;          // переменная для хранения радиуса рисуемых кругов
         Random rand;                // объект для получения случайных чисел
 
+        int[] alpha_value = new int[particles_amount]; //массив для хранения прозрачности кругов
+        int[,] coords = new int[2, particles_amount];  //массив для хранения координат кругов
+        int[] radiuses = new int[particles_amount];    //массив для хранения радиусов кругов
+        int outline_alpha = 20;                        //значение альфа-канала обводки
+
         //функция, которая рисует круг по координатам его центра
-        void DrawCircle(int x, int y)
+        private void DrawCircle(int x, int y, int r)
         {
             int xc, yc;
-            circle_radius = rand.Next(10, 100);
-            xc = x - circle_radius;
-            yc = y - circle_radius;
-            form_graphics.FillEllipse(figure_brush, xc, yc, circle_radius, circle_radius);
-
-            form_graphics.DrawEllipse(outline_pen, xc, yc, circle_radius, circle_radius);
-
+            xc = x - r;
+            yc = y - r;
+            buff_graphics.Graphics.FillEllipse(figure_brush, xc, yc, r, r);
+            buff_graphics.Graphics.DrawEllipse(outline_pen, xc, yc, r, r);
         }
+
         public Main_Form()
         {
             InitializeComponent();
             DoubleBuffered = true;
-        }
-
-        private void Paint_btn_Click(object sender, EventArgs e)
-        {
-            form_background_painter();
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+            this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            menuStrip1.BackColor = Color.Transparent;
+            main_graphics = this.CreateGraphics();           //задание графики с привязкой к форме
+            main_graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            buff_graphics = buff_graphics_context.Allocate(main_graphics, this.ClientRectangle);
         }
 
         private void form_background_painter()
         {
-            form_graphics = this.CreateGraphics();           //задание графики с привязкой к форме
-            outline_pen = new Pen(Color.White);              //задание цвета для карандаша 
-            figure_brush = new SolidBrush(Color.WhiteSmoke); //задание цвета для кисти
+            rand = new Random(); //инициализация объекта для псевдослучайных чисел
+            start_circles_draw();
+        }
 
-            rand = new Random();                             //инициализация объекта для псевдослучайных чисел
+        private void start_circles_draw()
+        {
+            outline_pen = new Pen(Color.FromArgb(20, 255, 255, 255)); //задание цвета для карандаша
 
+            fill_form_background();
+
+            //вызов функции для отрисовки круга с предварительным выбором координат центра круга
+            //значений прозрачностей и радиусов
+            for (int i = 0; i < particles_amount; i++)
+            {
+                alpha_value[i] = rand.Next(20, 40);
+                radiuses[i] = rand.Next(5, 150);
+
+                //задание цвета для кисти
+                figure_brush = new SolidBrush(Color.FromArgb(alpha_value[i], 255, 244, 244));
+
+                coords[0, i] = rand.Next(this.Width);
+                coords[1, i] = rand.Next(this.Height);
+                DrawCircle(coords[0, i], coords[1, i], radiuses[i]);
+            }
+
+            buff_graphics.Render(main_graphics);
+            timer1.Enabled = true;
+        }
+
+        private void fill_form_background()
+        {
             //создание и инициализация объекта кисти для отрисовки линейного градиента
             LinearGradientBrush gradient_brush = new LinearGradientBrush(ClientRectangle,
                                                                     Color.FromArgb(10, 29, 59),
                                                                     Color.FromArgb(14, 40, 64),
                                                                     LinearGradientMode.Horizontal);
 
+            ColorBlend color_blend = new ColorBlend();
+            color_blend.Colors = new Color[] { Color.FromArgb(10, 29, 59),
+                                               Color.FromArgb(11, 32, 61),
+                                               Color.FromArgb(12, 35, 62),
+                                               Color.FromArgb(13, 38, 63),
+                                               Color.FromArgb(14, 40, 64),};
+            color_blend.Positions = new float[] { (float)0.0, (float)0.2, (float)0.5, (float)0.8, (float)1.0 };
+            gradient_brush.InterpolationColors = color_blend;
+
             //закраска области рисования кистью линейного градиента
-            form_graphics.FillRectangle(gradient_brush, ClientRectangle.X, ClientRectangle.Y,
+            buff_graphics.Graphics.FillRectangle(gradient_brush, ClientRectangle.X, ClientRectangle.Y,
                                         ClientRectangle.Width, ClientRectangle.Height);
+        }
 
-            //вызов функции для отрисовки круга с предварительным выбором координат центра круга
-            int x, y;
-
-            for (int i = 0; i < 15; i++)
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 20; i++)
             {
-                x = rand.Next(this.Width);
-                y = rand.Next(this.Height);
-                DrawCircle(x, y);
+                fill_form_background();
+                if (outline_alpha != 0) outline_alpha--;
+                for (int j = 0; j < 15; j++)
+                {
+                    if (alpha_value[j] != 0) alpha_value[j]--;
+                    figure_brush = new SolidBrush(Color.FromArgb(alpha_value[j], 255, 244, 244));
+                    outline_pen = new Pen(Color.FromArgb(outline_alpha, 255, 255, 255));
+                    DrawCircle(coords[0, j], coords[1, j], radiuses[j]);
+                }
+                buff_graphics.Render(main_graphics);
             }
-            
-            //включение таймера
-            timer1.Enabled = true;
 
-           
-            form_graphics.Dispose();
+            buff_graphics.Render(main_graphics);
+            start_circles_draw();
         }
 
         private void Main_Form_Load(object sender, EventArgs e)
         {
-            List<PictureBox> picturebox = new List<PictureBox>();
-            DirectoryInfo directoryInfo = new DirectoryInfo("media\\logosmall.png");
-            var recentpics = directoryInfo.GetFiles().ToList();
-            var y = 10;
+            #region somecode
+            //List<PictureBox> picturebox = new List<PictureBox>();
+            //DirectoryInfo directoryInfo = new DirectoryInfo("media\\logosmall.png");
+            //var recentpics = directoryInfo.GetFiles().ToList();
+            //var y = 10;
 
-            foreach (var file in recentpics)
-            {
-                var pb = new PictureBox();
-                pb.Location = new Point(picturebox.Count * 120 + 20, y);
-                pb.Size = new Size(100, 120);
-                try
-                {
-                    pb.Image = Image.FromFile(file.FullName);
-                }
-                catch (OutOfMemoryException) { continue; }
-                pb.SizeMode = PictureBoxSizeMode.StretchImage;
-                flowLayoutPanel1.Controls.Add(pb);
-                picturebox.Add(pb);
-            }
-        }
-
-        private void Exit_Btn_Click(object sender, EventArgs e)
-        {
-            this.Close();
+            //foreach (var file in recentpics)
+            //{
+            //    var pb = new PictureBox();
+            //    pb.Location = new Point(picturebox.Count * 120 + 20, y);
+            //    pb.Size = new Size(100, 120);
+            //    try
+            //    {
+            //        pb.Image = Image.FromFile(file.FullName);
+            //    }
+            //    catch (OutOfMemoryException) { continue; }
+            //    pb.SizeMode = PictureBoxSizeMode.StretchImage;
+            //    flowLayoutPanel1.Controls.Add(pb);
+            //    picturebox.Add(pb);
+            //}
+            #endregion
         }
 
         private void Main_Form_Resize(object sender, EventArgs e)
@@ -108,6 +150,23 @@ namespace CourseWork
             this.Invalidate();
         }
 
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            main_graphics.Dispose();
+            buff_graphics.Dispose();
+            timer1.Enabled = false;
+            this.Close();
+        }
+
+        private void paintToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            form_background_painter();
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            timer1.Interval = trackBar1.Value;
+        }
     }
 }
 
