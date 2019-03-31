492a410a50284014727620d350c45898bb470296
vostro <vostro@K3Y_8R34K3R-PC>
2019-03-31 21:00:41 +0300

    Particles drawing added

--- CourseWork/Form1.Designer.cs
+++ CourseWork/Form1.Designer.cs
@@ -28,15 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.Exit_Btn = new System.Windows.Forms.Button();
             this.Paint_btn = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // Exit_Btn
             // 
-            this.Exit_Btn.Location = new System.Drawing.Point(57, 59);
+            this.Exit_Btn.Location = new System.Drawing.Point(27, 12);
             this.Exit_Btn.Name = "Exit_Btn";
             this.Exit_Btn.Size = new System.Drawing.Size(144, 49);
             this.Exit_Btn.TabIndex = 0;
@@ -46,7 +47,7 @@
             // 
             // Paint_btn
             // 
-            this.Paint_btn.Location = new System.Drawing.Point(255, 59);
+            this.Paint_btn.Location = new System.Drawing.Point(207, 12);
             this.Paint_btn.Name = "Paint_btn";
             this.Paint_btn.Size = new System.Drawing.Size(144, 49);
             this.Paint_btn.TabIndex = 1;
@@ -54,21 +55,23 @@
             this.Paint_btn.UseVisualStyleBackColor = true;
             this.Paint_btn.Click += new System.EventHandler(this.Paint_btn_Click);
             // 
-            // pictureBox1
+            // flowLayoutPanel1
             // 
-            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.No;
-            this.pictureBox1.Location = new System.Drawing.Point(30, 176);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(40, 40);
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 72);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(633, 251);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
             // 
             // Main_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(659, 332);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.Paint_btn);
             this.Controls.Add(this.Exit_Btn);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -78,7 +81,6 @@
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Main_Form_Load);
             this.Resize += new System.EventHandler(this.Main_Form_Resize);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -87,7 +89,8 @@
 
         private System.Windows.Forms.Button Exit_Btn;
         private System.Windows.Forms.Button Paint_btn;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- CourseWork/Form1.cs
+++ CourseWork/Form1.cs
@@ -2,11 +2,33 @@
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Windows.Forms;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace CourseWork
 {
     public partial class Main_Form : Form
     {
+        SolidBrush figure_brush;    //объект для заливки рисуемой фигуры
+        Pen outline_pen;            //объект для обводки рисуемой фигуры
+        Graphics form_graphics;     //объект графики
+
+        int circle_radius;          // переменная для хранения радиуса рисуемых кругов
+        Random rand;                // объект для получения случайных чисел
+
+        //функция, которая рисует круг по координатам его центра
+        void DrawCircle(int x, int y)
+        {
+            int xc, yc;
+            circle_radius = rand.Next(10, 100);
+            xc = x - circle_radius;
+            yc = y - circle_radius;
+            form_graphics.FillEllipse(figure_brush, xc, yc, circle_radius, circle_radius);
+
+            form_graphics.DrawEllipse(outline_pen, xc, yc, circle_radius, circle_radius);
+
+        }
         public Main_Form()
         {
             InitializeComponent();
@@ -17,17 +39,63 @@ namespace CourseWork
         {
             form_background_painter();
         }
+
         private void form_background_painter()
         {
-            Graphics MyFormGrap = this.CreateGraphics();
-            LinearGradientBrush gradBrush = new LinearGradientBrush(ClientRectangle, Color.Green, Color.Blue, LinearGradientMode.Horizontal);
-            MyFormGrap.FillRectangle(gradBrush, ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
-            MyFormGrap.Dispose();
+            form_graphics = this.CreateGraphics();           //задание графики с привязкой к форме
+            outline_pen = new Pen(Color.White);              //задание цвета для карандаша 
+            figure_brush = new SolidBrush(Color.WhiteSmoke); //задание цвета для кисти
+
+            rand = new Random();                             //инициализация объекта для псевдослучайных чисел
+
+            //создание и инициализация объекта кисти для отрисовки линейного градиента
+            LinearGradientBrush gradient_brush = new LinearGradientBrush(ClientRectangle,
+                                                                    Color.FromArgb(10, 29, 59),
+                                                                    Color.FromArgb(14, 40, 64),
+                                                                    LinearGradientMode.Horizontal);
+
+            //закраска области рисования кистью линейного градиента
+            form_graphics.FillRectangle(gradient_brush, ClientRectangle.X, ClientRectangle.Y,
+                                        ClientRectangle.Width, ClientRectangle.Height);
+
+            //вызов функции для отрисовки круга с предварительным выбором координат центра круга
+            int x, y;
+
+            for (int i = 0; i < 15; i++)
+            {
+                x = rand.Next(this.Width);
+                y = rand.Next(this.Height);
+                DrawCircle(x, y);
+            }
+            
+            //включение таймера
+            timer1.Enabled = true;
+
+           
+            form_graphics.Dispose();
         }
 
         private void Main_Form_Load(object sender, EventArgs e)
         {
+            List<PictureBox> picturebox = new List<PictureBox>();
+            DirectoryInfo directoryInfo = new DirectoryInfo("media\\logosmall.png");
+            var recentpics = directoryInfo.GetFiles().ToList();
+            var y = 10;
 
+            foreach (var file in recentpics)
+            {
+                var pb = new PictureBox();
+                pb.Location = new Point(picturebox.Count * 120 + 20, y);
+                pb.Size = new Size(100, 120);
+                try
+                {
+                    pb.Image = Image.FromFile(file.FullName);
+                }
+                catch (OutOfMemoryException) { continue; }
+                pb.SizeMode = PictureBoxSizeMode.StretchImage;
+                flowLayoutPanel1.Controls.Add(pb);
+                picturebox.Add(pb);
+            }
         }
 
         private void Exit_Btn_Click(object sender, EventArgs e)
