bea3a35b7194d75f064f087c7435a299d56f5aaa
vostro <vostro@K3Y_8R34K3R-PC>
2019-04-02 02:04:17 +0300

    Loading animation changed Greeting deleted Still need to make main menu

--- CourseWork/Form1.Designer.cs
+++ CourseWork/Form1.Designer.cs
@@ -29,22 +29,15 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 38);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(633, 285);
-            this.flowLayoutPanel1.TabIndex = 3;
-            this.flowLayoutPanel1.Visible = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 10);
+            this.button1.Location = new System.Drawing.Point(12, 348);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(97, 22);
             this.button1.TabIndex = 0;
@@ -56,16 +49,24 @@
             // timer1
             // 
             this.timer1.Enabled = true;
-            this.timer1.Interval = 500;
+            this.timer1.Interval = 5400;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(388, 263);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(107, 107);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
             // Main_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(659, 332);
+            this.ClientSize = new System.Drawing.Size(896, 382);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.flowLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Main_Form";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -73,14 +74,15 @@
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Main_Form_Load);
             this.Resize += new System.EventHandler(this.Main_Form_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- CourseWork/Form1.cs
+++ CourseWork/Form1.cs
@@ -14,14 +14,12 @@ namespace CourseWork
 {
     public partial class Main_Form : Form
     {
-        const string filepath = "media\\loading";
+        const string filepath = "media\\loading.gif";
         //SolidBrush figure_brush;    //объект для заливки рисуемой фигуры
         //Pen outline_pen;            //объект для обводки рисуемой фигуры
         BufferedGraphicsContext buff_graphics_context = BufferedGraphicsManager.Current;
         BufferedGraphics buff_graphics;     //графический буфер
         Graphics main_graphics;     //объект графики для отрисовки с привязкой к форме
-        Bitmap buff_bitmap;
-        Image img;
 
         //Random rand;                // объект для получения случайных чисел
 
@@ -48,36 +46,20 @@ namespace CourseWork
             main_graphics = this.CreateGraphics();           //задание графики с привязкой к форме
             main_graphics.SmoothingMode = SmoothingMode.AntiAlias;
             this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            buff_graphics = buff_graphics_context.Allocate(main_graphics, this.ClientRectangle);
         }
 
-        private void fill_form_background()
+        private void fill_form_background_start()
         {
-            ////создание и инициализация объекта кисти для отрисовки линейного градиента
-            //LinearGradientBrush gradient_brush = new LinearGradientBrush(ClientRectangle,
-            //                                                        Color.FromArgb(10, 29, 59),
-            //                                                        Color.FromArgb(14, 40, 64),
-            //                                                        LinearGradientMode.Horizontal);
-
-            //ColorBlend color_blend = new ColorBlend();
-            //color_blend.Colors = new Color[] { Color.FromArgb(10, 29, 59),
-            //                                   Color.FromArgb(11, 32, 61),
-            //                                   Color.FromArgb(12, 35, 62),
-            //                                   Color.FromArgb(13, 38, 63),
-            //                                   Color.FromArgb(14, 40, 64),};
-            //color_blend.Positions = new float[] { (float)0.0, (float)0.2, (float)0.5, (float)0.8, (float)1.0 };
-            //gradient_brush.InterpolationColors = color_blend;
-
-            ////закраска области рисования кистью линейного градиента
-            //main_graphics.FillRectangle(gradient_brush, ClientRectangle.X, ClientRectangle.Y,
-            //                            ClientRectangle.Width, ClientRectangle.Height);
-
-            for (int i = 0; i < 5; i++)
-                for (int j = 1; j < 4; j++) draw_greetings_loading(filepath + j.ToString() + ".png");
-            Thread.Sleep(500);
-            button1.Visible = true;
+            pictureBox1.Image = Image.FromFile(filepath);
+            pictureBox1.Size = Image.FromFile(filepath).Size;
+            pictureBox1.Location = new Point((this.ClientRectangle.Width / 2) - (pictureBox1.Width / 2),
+                                            (this.ClientRectangle.Height / 2) + (pictureBox1.Height - 2));
+            this.BackColor = Color.Black;
+            pictureBox1.BackColor = Color.Transparent;
         }
 
-        private void draw_greetings_loading(string path)
+        private void fill_form_background_onwork()
         {
             buff_graphics = buff_graphics_context.Allocate(main_graphics, this.ClientRectangle);
 
@@ -97,20 +79,13 @@ namespace CourseWork
             gradient_brush.InterpolationColors = color_blend;
 
             //закраска области рисования кистью линейного градиента
-            buff_graphics.Graphics.FillRectangle(gradient_brush, ClientRectangle.X, ClientRectangle.Y,
+            main_graphics.FillRectangle(gradient_brush, ClientRectangle.X, ClientRectangle.Y,
                                         ClientRectangle.Width, ClientRectangle.Height);
-
-            img = Image.FromFile(path);
-            buff_bitmap = new Bitmap(img, ClientRectangle.Width, ClientRectangle.Height);
-            buff_graphics.Graphics.DrawImage(buff_bitmap, new Point(0, 0));
-            buff_graphics.Render(main_graphics);
-            Thread.Sleep(500);
-            buff_graphics.Dispose();
         }
 
         private void Main_Form_Load(object sender, EventArgs e)
         {
-
+            fill_form_background_start();
         }
 
         private void Main_Form_Resize(object sender, EventArgs e)
@@ -127,7 +102,11 @@ namespace CourseWork
         private void timer1_Tick(object sender, EventArgs e)
         {
             timer1.Enabled = false;
-            fill_form_background();
+            pictureBox1.Image = null;
+            pictureBox1.Enabled = false;
+            pictureBox1.Visible = false;
+            fill_form_background_onwork();
+            button1.Visible = true;
         }
     }
 }
